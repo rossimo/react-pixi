6ed15b95fdb64689d70eadafe9dc5df85f2932a0
Ross Squires <ross.squires@gmail.com>
2017-09-15 13:29:55 -0400

    Allow first catch-up render

--- src/ReactPIXI.js
+++ src/ReactPIXI.js
@@ -460,20 +460,19 @@ var PIXIStage = createReactClass({
     );
     ReactUpdates.ReactReconcileTransaction.release(transaction);
     this.renderStage();
+    
+    var that = this;
+    that._rAFID = window.requestAnimationFrame( rapidrender );
 
-    if (!this.props.disableAutoRender) {
-      var that = this;
-      that._rAFID = window.requestAnimationFrame( rapidrender );
-
-      function rapidrender(timestamp) {
-
-          that._timestamp = timestamp;
-          that._rAFID = window.requestAnimationFrame( rapidrender );
-
-          // render the stage
-          that.renderStage();
+    function rapidrender(timestamp) {
+      that._timestamp = timestamp;
+      if (!this.props.disableAutoRender) {
+        that._rAFID = window.requestAnimationFrame( rapidrender );
       }
-    }
+
+      // render the stage
+      that.renderStage();
+      }
   },
 
   componentDidUpdate: function(oldProps) {
